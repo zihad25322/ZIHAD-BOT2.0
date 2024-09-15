1199700ce2477869a477cd6284163daf49f2c931
RANA <ruhulaminrana98@gmail.com>
2024-09-15 21:59:14 +0600

    Add files via upload

---
+++ scripts/commands/Lovestory.js
@@ -0,0 +1,42 @@
+module.exports.config = {
+  name: "lovestory", 
+  version: "1.0.0", 
+  permission: 0,
+  credits: "Imran Ahmed_x_Mahabub Rahman",
+  description: "example",
+  prefix: false,
+  category: "Fun", 
+  usages: "user", 
+  cooldowns: 5,
+  dependencies: {
+        "axios": "",
+        "fs-extra": ""
+  }
+};
+
+module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currencies}) => {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+   var hi = ["শুন একটি প্রেম এর গল্প।।\n\n MOHAMMAD RANA _۵ღ︵🩷🪽"];
+  var know = hi[Math.floor(Math.random() * hi.length)];
+  var link = [
+  "https://drive.google.com/uc?id=1Z-6qll7ACq8Ka1pKpnC3guGcXU0gNvoL",
+    "https://drive.google.com/uc?id=1YHm_oC7xItLokbt_MCWu-VdMGavvx-G4",
+    "https://drive.google.com/uc?id=1YvDanPpMZKK4s547h9Bsf0uL719AvVEG",
+    "https://drive.google.com/uc?id=1YemK-RQH3gUX_I9ThgNJLC89yPF-VtEY",
+    "https://drive.google.com/uc?id=1YN25UGQQCpZvN29Y5a84ZCYlL-rc_JL_",
+    "https://drive.google.com/uc?id=1YXbox7CyKc6Gu-56WAtAPlxSTs51Yo9n",
+    "https://drive.google.com/uc?id=1YNVe1cEM0JM3lj7sU49tuJn4-8ASYVd8",
+    "https://drive.google.com/uc?id=1ZDjeuPfIyUkovgmJCRsE7vE67aOe0Sp2",
+    "https://drive.google.com/uc?id=1YcJciCtidcUxRGihUyx2uDgDg3cBYUE5",
+    "https://drive.google.com/uc?id=1ZIE6xPwXg6_oxG0K7xCWKS04MNyag3QL",
+    "https://drive.google.com/uc?id=1ZF9cOD_fj26rpWDf6WOjUQNz8QuRJhkv",
+    "https://drive.google.com/uc?id=1ZAXQwA5BsnN555FrWii2bb-kdLaoMxLP",
+    "https://drive.google.com/uc?id=1YvWRk-wQ_I8kuJzOuw2Mx7Q-Rrgbw6CT",
+    "https://drive.google.com/uc?id=1Z8vKgEBUkKbwgMPvv_6K_lvVsrBca_X8",
+    "https://drive.google.com/uc?id=1ZG-BJq7pP4oh93U6tHIKuYvZ8XiidlCV",
+];
+     var callback = () => api.sendMessage({body:`「 ${know} 」`,attachment: fs.createReadStream(__dirname + "/cache/15.mp4")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/15.mp4"));    
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/15.mp4")).on("close",() => callback());
+   }
---
+++ scripts/commands/Rana-v.js
@@ -0,0 +1,86 @@
+const axios = require("axios");
+const fs = require("fs");
+const request = require("request");
+
+const link = [
+  "https://i.imgur.com/YZHtq60.mp4",
+  "https://i.imgur.com/1IeHri1.mp4",
+  "https://i.imgur.com/jQgBFcT.mp4",
+  "https://i.imgur.com/pbxDY0g.mp4",
+  "https://i.imgur.com/ADebnuP.mp4",
+  "https://i.imgur.com/wrUU3Dy.mp",
+  "https://i.imgur.com/y6ObvT3.mp4",
+  "https://i.imgur.com/jdIdLYo.mp4",
+  "https://i.imgur.com/RAxJxRQ.mp4",
+  "https://i.imgur.com/h5CCP73.mp4",
+  "https://i.imgur.com/lub1znh.mp4",
+  "https://i.imgur.com/QH3AWQS.mp4",
+  "https://i.imgur.com/gfbjgtP.mp4",
+  "https://i.imgur.com/lmfiBRp.mp4",
+  "https://i.imgur.com/vHmIR2a.mp4",
+  "https://i.imgur.com/PIHFTTz.mp4",
+  "https://i.imgur.com/BQOw4V2.mp4",
+  "https://i.imgur.com/Is9sLgx.mp4",
+  "https://i.imgur.com/FkB05ez.mp4",
+  "https://i.imgur.com/ZHFe1XF.mp4"
+];
+
+module.exports.config = {
+  name: "😒",
+  version: "1.0.0",
+  permission: 0,
+  credits: "Rahad",
+  description: "",
+  prefix: true, 
+  category: "no prefix", 
+  usages: "😒",
+  cooldowns: 5,
+  dependencies: {
+    "request": "",
+    "fs-extra": "",
+    "axios": ""
+  }
+};
+
+module.exports.handleEvent = async ({ api, event, Threads }) => {
+  if (event.body.startsWith("😒")) {
+    const rahad = [
+      "_ওই দিকে কি দেখো এ দিকে দেখো..!🫂😍",
+      " উম্মম্মমমমমমহহহ..বেবি ওই দিকে কি 😒"
+    
+    ];
+    const rahad2 = rahad[Math.floor(Math.random() * rahad.length)];
+
+    const callback = () => api.sendMessage({
+      body: `${rahad2}`,
+      attachment: fs.createReadStream(__dirname + "/cache/2024.mp4")
+    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/2024.mp4"), event.messageID);
+    
+    const requestStream = request(encodeURI(link[Math.floor(Math.random() * link.length)]));
+    requestStream.pipe(fs.createWriteStream(__dirname + "/cache/2024.mp4")).on("close", () => callback());
+    return requestStream;
+  }
+};
+
+module.exports.languages = {
+  "vi": {
+    "on": "Dùng sai cách rồi lêu lêu",
+    "off": "sv ngu, đã bão dùng sai cách",
+    "successText": `🧠`,
+  },
+  "en": {
+    "on": "on",
+    "off": "off",
+    "successText": "success!",
+  }
+};
+
+module.exports.run = async ({ api, event, Threads, getText }) => {
+  const { threadID, messageID } = event;
+  let data = (await Threads.getData(threadID)).data;
+  if (typeof data["😒"] === "undefined" || data["😒"]) data["😒"] = false;
+  else data["😒"] = true;
+  await Threads.setData(threadID, { data });
+  global.data.threadData.set(threadID, data);
+  api.sendMessage(`${(data["😒"]) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
+};
