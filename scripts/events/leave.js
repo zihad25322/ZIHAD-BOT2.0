33ca334e3b5c18b4b2c5605c77de689d3d7afefb
RANA <ruhulaminrana98@gmail.com>
2024-09-19 21:49:27 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -20,7 +20,7 @@ module.exports.run = async function({ api, event, Users, Threads }) {
 
 	if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-	(typeof data.customLeave == "undefined") ? msg = "তুই {name} গ্রুপে থাকার যোগ্য না আবাল .\n\n{type} " : msg = data.customLeave;
+	(typeof data.customLeave == "undefined") ? msg = " আমি রানা বট থাকতে তুই {name} কোথায় পালাবি 😌 .\n\n{type} " : msg = data.customLeave;
 	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
 
 	if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
