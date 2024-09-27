079ea0a8e3579f827c9870104fd70318aa59a878
zihad25322 <162394310+zihad25322@users.noreply.github.com>
2024-09-27 12:31:18 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -1,30 +1,39 @@
 module.exports.config = {
-	name: "leave",
-	eventType: ["log:unsubscribe"],
-	version: "1.0.0",
-	credits: "Nayan",
-	description: "notify leave.",
+  name: "leave",
+  eventType: ["log:unsubscribe"],
+  version: "1.0.0",
+  credits: "Nayan",
+  description: "notify leave.",
 };
 
 module.exports.run = async function({ api, event, Users, Threads }) {
-	if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
-	const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-	const { join } =  global.nodemodule["path"];
-	const { threadID } = event;
-	const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
-	const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-	const type = (event.author == event.logMessageData.leftParticipantFbId) ? " ~আগে আমার জিহাদ বসকে উম্মাহ্ দে তারপর লিভ নে 🙄" : "Kicked by Administrator";
-	const path = join(__dirname, "nayan", "leaveGif");
-	const gifPath = join(path, `l.gif`);
-	var msg, formPush
+  if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
 
-	if (existsSync(path)) mkdirSync(path, { recursive: true });
+  const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+  const { join } = global.nodemodule["path"];
+  const { threadID } = event;
+  const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
+  const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
+  const type = (event.author == event.logMessageData.leftParticipantFbId) ? "তোর কতো বড় সাহস আমি জিহাদ bot থাকতে তুই লিফট নিবি? থাপ্পর চিনো তুই☹️😡 🥱দারা তোরে আবার এড দিবো তোরে 🤨😁" : "বাল পাকনামির কারণে কিক খাইলো🤧";
+  const path = join(__dirname, "Nayan", "leavemp4");
+  const gifPath = join(path, `l.mp4`);
+  let msg, formPush;
 
-	(typeof data.customLeave == "undefined") ? msg = "~আমি জিহাদ বট থাকতে তুই {name} কোথায় পালাবি 😌 .\n\n{type} " : msg = data.customLeave;
-	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
+  // Create directory if it does not exist
+  if (!existsSync(path)) mkdirSync(path, { recursive: true });
 
-	if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
-	else formPush = { body: msg }
-	
-	return api.sendMessage(formPush, threadID);
-}
+  if (typeof data.customLeave === "undefined") {
+    msg = "ইস {name} {type} 🤖";
+  } else {
+    msg = data.customLeave;
+  }
+  msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
+
+  if (existsSync(gifPath)) {
+    formPush = { body: msg, attachment: createReadStream(gifPath) };
+  } else {
+    formPush = { body: msg };
+  }
+
+  return api.sendMessage(formPush, threadID);
+};
