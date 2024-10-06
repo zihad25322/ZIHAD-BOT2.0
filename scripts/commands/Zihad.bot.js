8f019bc15cb8d55614fbb519edb856600c442647
zihad25322 <162394310+zihad25322@users.noreply.github.com>
2024-10-06 15:36:09 +0600

    Update Zihad.bot.js

--- scripts/commands/Zihad.bot.js
+++ scripts/commands/Zihad.bot.js
@@ -17,7 +17,7 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
   const axios = require('axios')
-  const NAYAN = ['https://drive.google.com/file/d/1bfs8jry0oiW_X2HO3SSYinWSgmJHmKRh/view?usp=drivesdk']
+  const NAYAN = ['https://i.imgur.com/jP1JAtE.jpeg"https://i.imgur.com/CQsTNqY.jpeg']
     var rndm = NAYAN[Math.floor(Math.random() * NAYAN.length)];
 const media = (
     await axios.get(
@@ -26,13 +26,13 @@ const media = (
     )
   ).data;
 
-	if (body.indexOf("boss")==0 || body.indexOf("👍")==0 || body.indexOf("😍")==0 || body.indexOf("jan")==0 || body.indexOf("hi")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0) {
+	if (body.indexOf("boss")==0 || body.indexOf("👍")==0 || body.indexOf("😍")==0 || body.indexOf("jan")==0 || body.indexOf("hi")==0 || body.indexOf("jihad")==0 || body.indexOf("জিহাদ")==0 || body.indexOf("Sona")==0 || body.indexOf("bby")==0 || body.indexOf("xan")==0) {
 		var msg = {
 				body: "•─𝄞⋆⃝🌺𝐂`𝐄`𝐎  𝑻𝑶𝑴⎯⃝🌺🌿 ⋆⃝🐰🦋",
 				attachment: media
 			}
 			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("🖤", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🩷", event.messageID, (err) => {}, true)
 		}
 	},
 	start: function({ nayan }) {
