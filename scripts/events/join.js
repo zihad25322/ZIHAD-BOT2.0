712743c61250d561b20563341a546f17bb229799
zihad25322 <162394310+zihad25322@users.noreply.github.com>
2024-11-19 15:32:42 +0600

    Update join.js

--- scripts/events/join.js
+++ scripts/events/join.js
@@ -56,30 +56,25 @@ axios.get(gifUrl, { responseType: 'arraybuffer' })
     fs.writeFileSync(gifPath, response.data);
   if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
     api.changeNickname(`[ ${global.config.PREFIX} ] • ➠${(!global.config.BOTNAME) ? "bot" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
-    return api.sendMessage("⎯͢⎯⃝🤍জা্ঁন্ঁ মো্ঁই্ আ্ঁই্ঁসা্ঁ প্ঁর্ঁছি্ঁ জিঁঁহা্ঁদ্ঁএ্ঁর্ঁ ব্ঁট্ঁ তো্ঁমা্ঁদে্ঁর্ঁ সা্ঁথে্ঁ আঁলঁগাঁ পিঁরিঁতঁ কঁরঁতেঁ..🤭⎯͢⎯⃝😁", event.threadID, () => api.sendMessage({ body: `${global.config.BOTNAME} CONNECTED«\n\n🌱𝗔𝘀𝘀𝗮𝗹𝗮𝗺𝘂 𝗮𝗹𝗮𝗶𝗸𝘂𝗺🥀🌼
+    return api.sendMessage("⎯͢⎯⃝🤍জা্ঁন্ঁ মো্ঁই্ আ্ঁই্ঁসা্ঁ প্ঁর্ঁছি্ঁ জিঁঁহা্ঁদ্ঁএ্ঁর্ঁ ব্ঁট্ঁ তো্ঁমা্ঁদে্ঁর্ঁ সা্ঁথে্ঁ আঁলঁগাঁ পিঁরিঁতঁ কঁরঁতেঁ..🤭⎯͢⎯⃝😁🤭!", event.threadID, () => api.sendMessage({ body: `${global.config.BOTNAME} CONNECTED«\n\nAssalamualaykum☘️
 <------------------------------>  
-𝗕𝗼𝘁 𝗖𝗼𝗻𝗻𝗲𝗰𝘁𝗲𝗱 𝗦𝘂𝗰𝗰𝗲𝘀𝗳𝘂𝗹 !!! 
+BOT CONNECTED SUCCESFUL !!! 
 
-𝗔𝗽𝗽𝗿𝗼𝘃𝗮𝗹 𝗔𝗹𝗹𝗼𝘄 𝗜𝗻 𝗧𝗵𝗶𝘀 𝗚𝗿𝗼𝘂𝗽 !!!
+APPROVAL ALLOW IN THIS GROUP!!!
+<------------------------------>\n\nTO VIEW ANY COMMAND Use👉${global.config.PREFIX}help\nSee command👉 ${global.config.PREFIX}command
+\n\EXAMPLE:👇\n${global.config.PREFIX}admin (information)\n${global.config.PREFIX}islam (video)\n${global.config.PREFIX}tik (link)\n${global.config.PREFIX}fbvideo (link)
 <------------------------------>
+AND FOR ANY COMPLAINTS OR CONTACT BOT OPERATOR 
 
-𝗨𝘀𝗲 𝗛𝗲𝗹𝗽 𝗧𝗼 𝗦𝗲𝗲 𝗖𝗼𝗺𝗺𝗮𝗻𝗱 
-\n\n𝗨𝘀𝗲 ${global.config.PREFIX}𝗛𝗲𝗹𝗽 𝗧𝗼 𝗦𝗲𝗲 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀.\n\n𝗘𝘅𝗮𝗺𝗽𝗹𝗲:\n${global.config.PREFIX}𝗛𝗮𝗱𝗶𝘀(𝗧𝗲𝘅𝘁)\n${global.config.PREFIX}𝗦𝘁𝗮𝘁𝘂𝘀 (𝗦𝘁𝗮𝘁𝘂𝘀 𝘃𝗶𝗱𝗲𝗼)\n${global.config.PREFIX}𝗛𝗲𝗹𝗽 (𝗖𝗼𝗺𝗺𝗮𝗻𝗱)\n${global.config.PREFIX}𝗜𝗻𝗳𝗼 (𝗔𝗱𝗺𝗶𝗻)
-<<<<<------------------------------>>>>>
-𝗔𝗻𝗱 𝗙𝗼𝗿 𝗔𝗻𝘆 𝗥𝗲𝗽𝗼𝗿𝘁 𝗢𝗿 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 𝗕𝗼𝘁 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿
+DEVELOPER :Mohammad Zihad 
 
-۞ 𝘽𝙊𝙏 𝙒𝙊𝙉𝙀𝙍 : 𝗭𝗜𝗛𝗔𝗗 𝗜𝗦𝗟𝗔𝗠
+🟣Facebook Account Link: 
 
-✷ 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸: 
-https://www.facebook.com/100067540204855
+https://www.facebook.com/xxn.zihad
 
-❊ 𝗣𝗮𝗿𝘀𝗼𝗻𝗮𝗹 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : m.me/100067540204855
+🔵WHATSAPP NUMBER: wa.me/+8801876250734
 
-✲ 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺: m.me/XIHADZIHAD
-
-❁ 𝗘𝗺𝗮𝗶𝗹: zvai075@gmail.com
-
-✿ 𝗪𝗣: m.me/+8801734945665`, attachment: fs.createReadStream(gifPath)}, threadID));
+🟢SUPPORT EMAIL: zihad262940@gmail.com`, attachment: fs.createReadStream(gifPath)}, threadID));
   }})
 .catch(error => {
     console.error(error);
